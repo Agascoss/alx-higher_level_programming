61267f1a1305670aa9057c70b6fd6694396592d3
Agascoss <Danielagadzi302@gmail.com>
2023-10-25 13:18:47 -0700

    SQL

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+
+# displa the score of Bob to 10 in the table second_table.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";the number of times a specific IP and HTTP code is found in the apache log access file
